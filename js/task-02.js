292fcca16d4899d120de5f6343104e03f9fe6359
hallekh <lekhnovska.halyna@gmail.com>
2023-12-17 17:33:14 +0100

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.getElementById("ingredients");
+
+function createMarkup(arr) {
+  const items = arr.map((item) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.classList.add("item");
+
+    return li;
+  });
+  console.log(items);
+  list.append(...items);
+}
+
+createMarkup(ingredients);
