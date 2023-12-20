6343a975c24915cdf87a44b17cc90c22b123676a
hallekh <lekhnovska.halyna@gmail.com>
2023-12-20 10:10:36 +0100

    task-1 changes

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
 const categoriesList = document.getElementById("categories");
-const categoriesItems = categoriesList.querySelectorAll(".item");
+const categoriesItems = categoriesList.children;
 
 console.log(`Number of categories: ${categoriesItems.length}`);
 
-categoriesItems.forEach((category) => {
+for (const category of categoriesItems) {
   const categoryName = category.querySelector("h2").textContent;
-  const categoryElements = category.children("ul li");
+  const categoryElements = category.querySelector("ul").children;
 
   console.log(`\nCategory: ${categoryName}`);
   console.log(`Elements: ${categoryElements.length}`);
-});
+}
